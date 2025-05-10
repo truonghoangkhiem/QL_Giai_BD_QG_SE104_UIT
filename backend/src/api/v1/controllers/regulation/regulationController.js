0d064734f939ddf94b1613fb75c42f74013319eb
unknown <truongkhiemhoang@gmail.com>
2025-05-10 12:29:01 +0700

    update raking rule , remove !rule.point

--- backend/src/api/v1/controllers/regulation/regulationController.js
+++ backend/src/api/v1/controllers/regulation/regulationController.js
@@ -51,9 +51,6 @@ const validateRules = (regulation_name, rules) => {
         typeof rules.losePoints !== "number" ||
         rules.winPoints <= rules.drawPoints ||
         rules.drawPoints <= rules.losePoints ||
-        !rules.winPoints ||
-        !rules.drawPoints ||
-        !rules.losePoints ||
         !Array.isArray(rules.rankingCriteria)
       )
         return false;
